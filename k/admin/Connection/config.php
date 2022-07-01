59f776b524819582fe5f28f063d053154455d7b9
samiemaduka <madukasamuel@aol.com>
2022-07-01 13:39:32 +0100

    removed commit

--- k/admin/Connection/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'database_test');
-    define('DB_PASSWORD', 'password_test');
-    define('DB_DATABASE', 'database_test');
-?>
--- k/js/gtranslate.js
+++
@@ -1,7 +0,0 @@
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({
-        pageLanguage: 'en',
-        autoDisplay: 'true',
-        layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
-        }, 'google_translate_element');
-    }
