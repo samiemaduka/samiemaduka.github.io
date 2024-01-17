bcb9d667f949247dd1cd9ae00c4a28635c6cf5e4
samiemaduka <madukasamuel@aol.com>
2024-01-17 02:36:38 +0100

    Update sidebar.js

--- newcou/js/sidebar.js
+++ newcou/js/sidebar.js
@@ -1,11 +1,11 @@
 // sidebar javascript starts
 function openNav() {
-    document.getElementById("mySidebar").style.width = "17rem";
+    document.getElementById("mySidebar").style.height = "18rem";
     document.getElementById("main").style.marginLeft = "0";
 }
   
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("mySidebar").style.height = "0px";
     document.getElementById("main").style.marginLeft= "0px";
 }
 // sidebar javascript ends
